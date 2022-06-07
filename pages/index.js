6846145dfb7a4bba1ef4dc7f46e9ad580beefb31
Eddy Guo <eddyyanruguo@gmail.com>
2022-06-07 18:17:55 -0400

    favicon 2

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Head>
         <title>Eddy Guo</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon.ico" />
       </Head>
       <ul>
         <li>
