f8b1e2d7116d626ab79632690f7d20d8f74215f8
Eddy Guo <eddyyanruguo@gmail.com>
2022-06-07 02:45:29 -0400

    format

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,45 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Head><title>Eddy Guo</title></Head>
-        <h1>Hey, I'm Eddy Guo &#127837;</h1>
-        <p>I'm a 3rd year student studying Computer Science at the University of Waterloo. I am currently exploring blockchain and web3 development.<br/><br/>
-        I have previously developed software at <Link href="https://atsautomation.com/"><a target="_blank">ATS Automation</a></Link>
-        {' '}and <Link href="https://www.processfusion.com/"><a target="_blank">Process Fusion</a></Link>.<br/></p>
-        <ul>
-        <li><Link href="/research"><a>Research</a></Link></li>
-        <li><a href="/Resume_Eddy_Guo.pdf" target="_blank" rel="noopener noreferrer">Resume</a></li>
-        <li><a href="https://github.com/eddy-guo" target="_blank">Github</a></li>
-        </ul>
+      <Head>
+        <title>Eddy Guo</title>
+      </Head>
+      <h1>Hey, I'm Eddy Guo &#127837;</h1>
+      <p>
+        I'm a 3rd year student studying Computer Science at the University of
+        Waterloo. I am currently exploring blockchain and web3 development.
+        <br />
+        <br />I have previously developed software at{" "}
+        <Link href="https://atsautomation.com/">
+          <a target="_blank">ATS Automation</a>
+        </Link>{" "}
+        and{" "}
+        <Link href="https://www.processfusion.com/">
+          <a target="_blank">Process Fusion</a>
+        </Link>
+        .<br />
+      </p>
+      <ul>
+        <li>
+          <Link href="/research">
+            <a>Research</a>
+          </Link>
+        </li>
+        <li>
+          <a
+            href="/Resume_Eddy_Guo.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/eddy-guo" target="_blank">
+            Github
+          </a>
+        </li>
+      </ul>
     </>
   );
 }
--- pages/research.js
+++ pages/research.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
 export default function MEV() {
   return (
@@ -8,9 +8,22 @@ export default function MEV() {
         <title>Research | Eddy Guo</title>
       </Head>
       <h1>Web3 Research</h1>
-      <p>I am a developer and analyst for the UW Blockchain Club - to keep track of what I'm learning and hold myself accountable, I will try my best to update this page consistently &#128526;</p>
-      <p><Link href="/research/MEV"><a>MEV</a></Link>
-      <br/><br/><br/><Link href="/"><a>Back to home</a></Link></p>
+      <p>
+        I am a developer and analyst for the UW Blockchain Club - to keep track
+        of what I'm learning and hold myself accountable, I will try my best to
+        update this page consistently &#128526;
+      </p>
+      <p>
+        <Link href="/research/MEV">
+          <a>MEV</a>
+        </Link>
+        <br />
+        <br />
+        <br />
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </p>
     </>
   );
 }
