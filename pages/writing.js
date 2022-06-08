5c05b5774771dc4232e859d55d2bb0cfcf54ff1d
Eddy Guo <eddyyanruguo@gmail.com>
2022-06-08 01:04:36 -0400

    quote text opacity

--- pages/writing.js
+++ pages/writing.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import reactDom from "react-dom";
 
 export default function Writing() {
   return (
@@ -9,9 +10,7 @@ export default function Writing() {
       </Head>
       <h1>Writing Pieces</h1>
       <p>
-        "Writing is timeless" - Anish Agnihotri
-        <br />
-        <br />
+        <p style={{color:"#091930", opacity:"0.5"}}>"Writing is timeless" - Anish Agnihotri</p>
         As I progress through college and the rest of my life, I will explore
         various career paths, meet many people, and understand more about
         myself. Keeping track of interesting events and what I learned would be
