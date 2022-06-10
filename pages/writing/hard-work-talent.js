7a5b244b1b8eff484aad66df5a51e505aac0528e
Eddy Guo <eddyyanruguo@gmail.com>
2022-06-09 21:13:04 -0400

    remove placeholder

--- pages/writing/hard-work-talent.js
+++ pages/writing/hard-work-talent.js
@@ -8,15 +8,7 @@ export default function hardWorkTalent() {
         <title>Hard Work vs. Talent | Eddy Guo</title>
       </Head>
       <h1>Hard Work vs. Talent - A cup-stacking case study</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
+      <p>Coming soon!</p>
       <ul className="bottom">
         <li>
           <Link href="/writing">
