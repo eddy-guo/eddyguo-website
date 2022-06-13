d73dd5d28da42a57e31eb651a632367db0e1c754
Eddy Guo <eddyyanruguo@gmail.com>
2022-06-13 16:34:20 -0400

    favicon fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Head from "next/head";
 import "../styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
