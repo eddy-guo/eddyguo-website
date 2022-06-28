5525aded8ce41cc586f218a02abaf4e8125e6cb2
Eddy Guo <eddyyanruguo@gmail.com>
2022-06-28 03:49:46 -0400

    todo list for snip3r

--- pages/projects/snip3r.js
+++ pages/projects/snip3r.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function snipe() {
   return (
@@ -9,12 +8,20 @@ export default function snipe() {
         <title>snip3r | Eddy Guo</title>
       </Head>
       <h1>
-        snip3r<img className="logo" src="/logo.png" alt="snip3r"></img>
+        snip3r
+        <img className="logo" src="/logo.png" alt="snip3r"></img>
         <img className="shooter" src="/shooter.png" alt="shooter"></img>
         <br />
         (ETHNewYork blockchain hackathon)
       </h1>
-
+      <p>
+        <Link href="https://ethglobal.com/showcase/snip3r-0zmy2">
+          <a target="_blank">link</a>
+        </Link>
+        <br />
+        <br />
+        Todo: writeup, finish code, event details
+      </p>
       <ul className="bottom">
         <li className="button">
           <Link href="/projects">
