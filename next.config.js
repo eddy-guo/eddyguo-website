4c438fecc2bef991d867fff599f9d606237da178
Eddy Guo <eddyyanruguo@gmail.com>
2022-10-03 01:42:08 -0400

    pkg + config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-  experimental: {
-    scrollRestoration: true,
-    images: {
-      allowFutureImage: true
-    }
-  },
-};
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  /* config options here */
+}
+
+module.exports = nextConfig
