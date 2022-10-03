5492aa5ccb59317d8393f5a3324e6e9d9667b78a
Eddy Guo <eddyyanruguo@gmail.com>
2022-10-03 00:51:19 -0400

    test

--- pages/projects/snip3r.js
+++ pages/projects/snip3r.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/future/image";
-
+import Image from "next/image";
 export default function snipe() {
   return (
     <>
